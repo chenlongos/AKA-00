76258d4c53160fecf3349826384cd6af43284b5c
junbo.dai <bobodai515@163.com>
2026-02-12 11:24:10 +0800

    chore: delete useless CarSimPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import {Route, Routes} from "react-router-dom";
 import BaseControlPage from "./pages/BaseControlPage.tsx";
 import SimPage from "./pages/SimPage.tsx";
-import CarSimPage from "./pages/CarSimPage.tsx";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<BaseControlPage/>}/>
             <Route path="/sim" element={<SimPage/>}/>
-            <Route path="/car" element={<CarSimPage/>}/>
         </Routes>
     )
 }
