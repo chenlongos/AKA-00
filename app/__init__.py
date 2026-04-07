687d55548a365f98cbddc44e4f13e8ab3673c8d5
junbo.dai <bobodai515@163.com>
2026-04-07 21:48:19 +0800

    feat: suppose cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, request
 
 
 def create_app():
@@ -8,9 +8,26 @@ def create_app():
     from .routes.wifi import wifi_bp
     from .routes.frontend import frontend_bp
 
+    @app.before_request
+    def handle_cors_preflight():
+        if request.method == "OPTIONS":
+            return _with_cors_headers(make_response("", 204))
+
+    @app.after_request
+    def add_cors_headers(response):
+        return _with_cors_headers(response)
+
     init_control_service(app)
     app.register_blueprint(api_bp, url_prefix="/api")
     app.register_blueprint(wifi_bp)  # WiFi 路由注册到根路径
     app.register_blueprint(frontend_bp)
 
     return app
+
+
+def _with_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Max-Age"] = "86400"
+    return response
